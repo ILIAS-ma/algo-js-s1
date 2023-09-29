a03784bc14eb2085d1ac16bb1360768c78459f4b
ILIAS-ma <ilias.bounabat@edu.devinci.fr>
2023-09-29 09:32:18 +0000

    mise a jour de mon folder

---
+++ Jeu_Pokemon/main.js
@@ -0,0 +1,24 @@
+class pokemon {
+    constructor( name ,  attack , defense ,  pv  ){
+       this.name = name 
+       this.attack = attack,
+       this.defense = defense,
+       this.pv = pv,
+    }
+} 
+let salameche = new pokemon("salameche", 120 , 50 ,300)
+console.log("Salameche")
+let Mortanon = new pokemon("mortanon" ,60 , 70 , 500 )
+console.log("Mortanon")
+
+while(salameche.pv > 0 && Mortanon > 0 )
+salameche.attack + "Mortanon se prend des dégats " 
+Mortanon.attack + "Salameche se prend des dégats "
+salameche.attack
+while (  Mortanon.pv > 0 ) {
+  console.log("attack")
+  Mortanon.pv = Mortanon.pv - 120 ; 
+  for( var i = 0 ; i > 10 ; i++ )
+
+}
+
